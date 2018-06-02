5f594ac6b88c55370a6e62feb4dffd05ab40eb6e
Dor Bar <dor4231@gmail.com>
2018-06-02 11:50:34 +0300

    feat: Click on cat image will increase player counter

---
+++ js/main.js
@@ -0,0 +1,24 @@
+class Player {
+    constructor() {
+        this.counter = 0;
+    }
+
+    increasCounter(num) {
+        this.counter += num;
+    }
+}
+
+const player = new Player();
+const catImage = document.querySelector(".cat-image");
+const clickCounter = document.querySelector(".click-counter .number");;
+
+
+
+function updatePageElements() {
+    clickCounter.innerText = player.counter;
+}
+
+catImage.addEventListener("click", function(event) {
+    player.increasCounter(1);
+    updatePageElements();
+});
