f8b5b4f9773d05b127125f990ac53b30327160cc
Dor Bar <dor4231@gmail.com>
2018-06-02 12:14:49 +0300

    feat: Wait for html page to load

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-class Player {
-    constructor() {
-        this.counter = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    class Player {
+
+        constructor() {
+            this.counter = 0;
+        }
+
+        increasCounter(num) {
+            this.counter += num;
+        }
     }
 
-    increasCounter(num) {
-        this.counter += num;
+    const player = new Player();
+    const catImage = document.querySelector(".cat-image");
+    const clickCounter = document.querySelector(".click-counter .number");
+
+
+    function updatePageElements() {
+        clickCounter.innerText = player.counter;
     }
-}
 
-const player = new Player();
-const catImage = document.querySelector(".cat-image");
-const clickCounter = document.querySelector(".click-counter .number");;
-
-
-
-function updatePageElements() {
-    clickCounter.innerText = player.counter;
-}
-
-catImage.addEventListener("click", function(event) {
-    player.increasCounter(1);
-    updatePageElements();
+    catImage.addEventListener("click", function (event) {
+        player.increasCounter(1);
+        updatePageElements();
+    });
 });
