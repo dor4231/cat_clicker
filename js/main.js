cbb36207ff7a8167369281160c9d25655aaaddac
Dor Bar <dor4231@gmail.com>
2018-06-03 22:35:04 +0300

    feat: Cat class

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    class Player {
+class Player {
 
-        constructor() {
-            this.counter = 0;
-        }
-
-        increasCounter(num) {
-            this.counter += num;
-        }
+    constructor() {
+        this.counter = 0;
     }
+
+    increasCounter(num) {
+        this.counter += num;
+    }
+}
+
+class Cats {
+    constructor(name, image) {
+        this.name = name;
+        this.image = `assets/img/${image}.jpg`;
+        this.catPanel = `<div class="cat-card"><h3>${this.name}</h3><img src="${this.image}" alt="Cat Image to click on"></div>`
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+
 
     const player = new Player();
     const catImage = document.querySelector(".cat-image");
