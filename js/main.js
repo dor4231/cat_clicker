998b384f669ccb7decbee9cc80701c805c5fbe20
Dor Bar <dor4231@gmail.com>
2018-06-03 23:02:34 +0300

    feat: Add cats to the page (card-container)

--- js/main.js
+++ js/main.js
@@ -9,22 +9,38 @@ class Player {
     }
 }
 
-class Cats {
+class Cat {
     constructor(name, image) {
         this.name = name;
         this.image = `assets/img/${image}.jpg`;
-        this.catPanel = `<div class="cat-card"><h3>${this.name}</h3><img src="${this.image}" alt="Cat Image to click on"></div>`
+        this.clickCounter = 0;
+        this.catCard = `
+    <div class="cat-card ${this.name}">
+        <h3>${this.name}</h3>
+        <p class="click-counter">Number of clicks: <span class="number"></span></p>
+        <img src="${this.image}" alt="Cat Image to click on">
+     </div>`;
     }
 }
 
 
 document.addEventListener("DOMContentLoaded", function () {
-
-
-
-
     const player = new Player();
-    const catImage = document.querySelector(".cat-image");
+    const catsImages = ['engry_cat', 'heary_cat'];
+    const catsNames = ['Mario', 'Marco'];
+    const cats = {};
+
+    const catsCardContainer = document.querySelector('.card-container');
+
+    for (const cat in catsNames) {
+        cats[catsNames[cat]] = new Cat(catsNames[cat], catsImages[cat]);
+    }
+
+    for (const cat of catsNames) {
+        catsCardContainer.innerHTML += cats[cat].catCard;
+    }
+
+
     const clickCounter = document.querySelector(".click-counter .number");
 
 
@@ -32,7 +48,12 @@ document.addEventListener("DOMContentLoaded", function () {
         clickCounter.innerText = player.counter;
     }
 
-    catImage.addEventListener("click", function (event) {
+    catsCardContainer.addEventListener("click", function (event) {
+        if(event.target.tagName === "DIV"){
+            const catName = event.target.querySelector('h3').innerText;
+            const catImage = document.querySelector(".cat-image");
+        }
+
         player.increasCounter(1);
         updatePageElements();
     });
